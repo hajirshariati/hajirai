30da627bed97173870657c5ed328a9cd9c0eae6c
ruslan <rbezuhlov@golim.com>
2025-10-06 18:27:47 +0300

    product signup banner

--- assets/product-signup.js
+++ assets/product-signup.js
@@ -30,11 +30,53 @@ class ProductSignUp extends HTMLElement {
   onSubmit(e) {
     e.preventDefault();
     this.submitBtn.classList.add("loading");
+    const formData = new FormData(this.signUpForm);
+    const formDataObject = Object.fromEntries(formData);
+    const firstName = formDataObject.first_name || "";
+    const lastName = formDataObject.last_name || "";
+    const email = formDataObject.email;
+    const checkbox = formDataObject.checkbox == "on" ? true : false;
+    console.log(firstName, lastName, email, checkbox);
+    fetch(
+      "https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-2321b64e-f816-4415-acfa-9ebbd8824f76/node-fetch/create-customer",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          firstName: firstName,
+          lastName: lastName,
+          marketing: checkbox,
+        }),
+      }
+    )
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (data.message == "Already a Member") {
+          this.onError(data);
+          return;
+        }
+        this.onSuccess(data);
+      })
+      .catch((err) => {
+        console.error("❌ Fetch error:", err);
+      });
   }
 
-  onSuccess(e) {
+  onSuccess(data) {
     this.classList.remove("active");
     this.classList.add("success");
+  }
+
+  onError(data) {
+    this.querySelector(".p-signup__error-heading").innerText = data.message;
+    this.classList.remove("active");
+    this.classList.add("error");
   }
 }
 
